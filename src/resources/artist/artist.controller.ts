51584f2e4af19bf91896ed25c29a4047c60488c1
Andrey Loginov <andrey12q112@gmail.com>
2024-03-11 22:15:15 +0300

    feat: implement artist endpoint

--- src/resources/artist/artist.controller.ts
+++ src/resources/artist/artist.controller.ts
@@ -7,6 +7,7 @@ import {
   Delete,
   Put,
   Header,
+  HttpCode,
 } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
@@ -18,6 +19,7 @@ export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
+  @HttpCode(201)
   @Header(JSON_HEADER_NAME, JSON_HEADER_VALUE)
   create(@Body() createArtistDto: CreateArtistDto) {
     return this.artistService.create(createArtistDto);
@@ -32,18 +34,19 @@ export class ArtistController {
   @Get(':id')
   @Header(JSON_HEADER_NAME, JSON_HEADER_VALUE)
   findOne(@Param('id') id: string) {
-    return this.artistService.findOne(+id);
+    return this.artistService.findOne(id);
   }
 
   @Put(':id')
   @Header(JSON_HEADER_NAME, JSON_HEADER_VALUE)
   update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
-    return this.artistService.update(+id, updateArtistDto);
+    return this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   @Header(JSON_HEADER_NAME, JSON_HEADER_VALUE)
   remove(@Param('id') id: string) {
-    return this.artistService.remove(+id);
+    return this.artistService.remove(id);
   }
 }
